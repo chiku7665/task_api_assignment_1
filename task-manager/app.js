0947feaa24ac651d78e7ea1386bd708796bf74d9
praveen krishna <praveenkrishna7665@gmail.com>
2024-02-10 09:34:14 +0530

    get implemented

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const taskData=require('./task.json')
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/tasks",(req, res)=>{
+  return res.status(200).send(taskData);
+});
+
+app.get("/tasks/:id",(req, res)=>{
+    const tasks=taskData.tasks;
+    let filteredTask=tasks.filter(task => task.id==req.params.id);
+    
+    if(filteredTask.length==0){
+        return res.status(404).send("Not found");
+    }
+    return res.status(200).send(filteredTask);
+  });
 
 app.listen(port, (err) => {
     if (err) {
