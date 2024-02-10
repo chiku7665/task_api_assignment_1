22345bbebe863fb8638a28dbfb8fbff12ae37054
praveen krishna <praveenkrishna7665@gmail.com>
2024-02-10 10:39:03 +0530

    post added

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const taskData=require('./task.json')
+const taskData=require('./task.json');
+const validator=require('./validator');
+const fs=require('fs');
+
+
 const app = express();
 const port = 3000;
 
@@ -19,6 +23,25 @@ app.get("/tasks/:id",(req, res)=>{
     }
     return res.status(200).send(filteredTask);
   });
+
+  app.post('/tasks',(req, res)=>{
+    const taskDetail=req.body;
+    let val=validator.validateTaskInfo(taskDetail);
+    const modifyTask=taskData.tasks;
+    if(val.status==true){
+      modifyTask.push(taskDetail);
+      fs.writeFile('./task-manager/task.json',JSON.stringify(modifyTask),{encoding:'utf8',flag:'w'},(err,data)=>{
+        if(err){
+            return res.status(500).send("something went wrong");
+        }
+        else{
+            return res.status(200).send("data is added");
+        }
+      })
+
+    }
+
+  })
 
 app.listen(port, (err) => {
     if (err) {
